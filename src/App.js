f6143a0993aa77993b461d7eb7a38d43d99bf71d
Alan Curtis <talancurtis@gmail.com>
2018-01-23 17:10:29 -0700

    finished afternoon project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+import { Link} from 'react-router-dom';
+import routes from './routes';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
-        </nav>
-      </div>
+        <div>
+          <nav className='nav'>
+            <div>WestSide University</div> 
+            <div className='link-wrap'>
+                <Link className='links' to='/'>Home</Link>
+                <Link className='links' to='/About'>About</Link> 
+            </div>
+          </nav>
+          {routes}          
+        </div>
     )
   }
 }
